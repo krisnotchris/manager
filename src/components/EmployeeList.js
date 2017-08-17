63942bfcb35acc7604708faff87cab02c5d52ea9
Kris Henderson <kris@vapewild.com>
2017-08-17 13:26:46 -0500

    update

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.employees);
+  }
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component will be rendered with.
+    // this.props is the old set of props
   }
   
   render() {
@@ -34,4 +44,8 @@ class EmployeeList extends Component {
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = (state) => {
+
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
