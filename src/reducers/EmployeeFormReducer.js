c9ad02af4be555c0e719b0ec35df3b99c4c81309
Kris Henderson <kris@Kriss-MacBook-Pro-3.local>
2017-08-16 21:58:21 -0500

    Fill out EmployeeFormReducer

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,11 +1,11 @@
 import { EMPLOYEE_UPDATE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = { name: '', phone: '', shift: '' };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMPLOYEE_UPDATE:
-            return null;
+            return { ...state, [action.payload.prop]: action.payload.value };
         default:
             return state;
     }
