a9e44cab783c199f0b388b6e16d39cd05a3f5fbc
Kris Henderson <kris@vapewild.com>
2017-08-16 21:16:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import Router from './Router';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBrIBVhAaUvIWYqkjPPen7t6uSz0BQ4gdQ',
+      authDomain: 'manager-1778e.firebaseapp.com',
+      databaseURL: 'https://manager-1778e.firebaseio.com',
+      projectId: 'manager-1778e',
+      storageBucket: 'manager-1778e.appspot.com',
+      messagingSenderId: '930435640231'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+        <Router />
+      </Provider>
+    );
+  }
+}
+
+export default App;
