8e425d2982d7659a9138185b224ff2c34a3c3d37
Kris Henderson <kris@vapewild.com>
2017-08-17 13:57:52 -0500

    transform the data object to an array for iteration

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
@@ -7,18 +8,23 @@ class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
 
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(this.props.employees);
+    this.createDataSource(this.props);
   }
   componentWillReceiveProps(nextProps) {
     // nextProps are the next set of props that this component will be rendered with.
     // this.props is the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(employees);
   }
   
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>
@@ -45,7 +51,10 @@ class EmployeeList extends Component {
 }
 
 const mapStateToProps = (state) => {
-
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+  return { employees };
 };
 
 export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
