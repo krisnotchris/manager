ad41126d31d9c7ab53f922b2d7f05cb3d9f541f6
Kris Henderson <kris@Kriss-MacBook-Pro-3.local>
2017-08-17 22:14:01 -0500

    done

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types';
+import { EMPLOYEE_UPDATE, 
+    EMPLOYEE_CREATE, 
+    EMPLOYEES_FETCH_SUCCESS, 
+    EMPLOYEE_SAVE_SUCCESS } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
@@ -29,6 +32,31 @@ export const employeesFetch = () => {
         firebase.database().ref(`/users/${currentUser.uid}/employees`)
         .on('value', snapshot => {
             dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+        });
+    };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+            .set({ name, phone, shift })
+            .then(() => { 
+                dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+                Actions.employeeList({ type: 'reset' });
+            });
+    };
+};
+
+export const employeeDelete = ({ uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+        .remove()
+        .then(() => {
+            Actions.employeeList({ type: 'reset' });
         });
     };
 };
